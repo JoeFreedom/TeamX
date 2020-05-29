e0e079e9b9bad6e64cce7044481ff7df10fc97d4
JoeFreedom <39825386+JoeFreedom@users.noreply.github.com>
2020-05-29 22:29:11 +0300

    Defined Functions and Divided Tasks among members

--- ConsoleFunctions.cs
+++ ConsoleFunctions.cs
@@ -4,8 +4,33 @@ using System.Text;
 
 namespace ConsoleFilesDirectories
 {
-    public class ConsoleFunctions
+    public static class ConsoleFunctions
     {
         // достаточно показать содержимое каталога, скопировать, удалить и создать, перейти к нужному каталогу
+        public static void DirectoryContent(string content)
+        {
+            // Sergey
+        }
+
+        public static void Copy()
+        {
+            // Lev
+        }
+
+        public static void Delete()
+        {
+
+        }
+
+        public static void Create()
+        {
+            // Joe
+        }
+
+        public static void GoToNextContent()
+        {
+
+        }
+       
     }
 }

5f929f3cc3d091051ed2e254068f100e1f0e4514
JoeFreedom <39825386+JoeFreedom@users.noreply.github.com>
2020-05-29 22:03:17 +0300

    Initialization
    Created class ConsoleFunctions and Program.cs

---
+++ ConsoleFunctions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleFilesDirectories
+{
+    public class ConsoleFunctions
+    {
+        // достаточно показать содержимое каталога, скопировать, удалить и создать, перейти к нужному каталогу
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using static System.Console;
+namespace ConsoleFilesDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

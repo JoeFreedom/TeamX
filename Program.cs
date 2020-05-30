454cb64f5873968a653f8a52afe30f286e363580
SergeyOlisov <58857959+SergeyOlisov@users.noreply.github.com>
2020-05-30 18:45:26 +0300

    Clear

--- Program.cs
+++ Program.cs
@@ -8,19 +8,7 @@ namespace ConsoleFilesDirectories
     {
         static void Main(string[] args)
         {
-            //Тестирование методов
-            //Показать содержимое каталога:
-            DriveInfo[] drives = DriveInfo.GetDrives();
-            string path = "C:\\";
-            ConsoleFunctions.DirectoryContent(path); //Done!
-
-            //Удаление:
-            path = "C:\\TestFolder";
-            ConsoleFunctions.Delete(path); //Done!
-
-            //Переход в следующий каталог:
-            path = "C:\\";
-            ConsoleFunctions.GoToNextContent(path); //Done!
+            
         }
     }
 }

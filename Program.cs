193c7cd4d6733ea11c61994ee539ab4ebe90b016
JoeFreedom <39825386+JoeFreedom@users.noreply.github.com>
2020-05-31 15:01:17 +0300

    Programme Ready!
    Version 0.1B

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
-﻿using System;
+using System;
 using System.IO;
-
 using static System.Console;
+
 namespace ConsoleFilesDirectories
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            string path = @"C:\";
+            bool end = false;
+            WriteLine("Input 'Info' to see the menu.");
+            do
+            {
+                ConsoleFunctions.ShowMenu(ref path, ref end);
+            } while (!end);
+            ReadKey();
         }
     }
 }
